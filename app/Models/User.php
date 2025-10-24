a8ee9a1f2bd3d2617dfe14e4507ffd744399935d
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-10-24 20:01:53 +0300

    init Filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,18 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Enums\Role;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Builder;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles;
@@ -45,5 +51,22 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function isAdmin(): Attribute
+    {
+        return Attribute::get(fn (): bool => $this->hasRole(Role::admin->value));
+    }
+
+    public function scopeAdmins(Builder $query): Builder
+    {
+        return $query->whereHas('roles', function (Builder $query) {
+            $query->where('name', Role::admin->value);
+        });
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->is_admin;
     }
 }
